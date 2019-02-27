2c2120244a40ed18c31e9ec04a9f0e9a0420a0e2
Paw Høvsgaard <pawhovsgaard@gmail.com>
2019-02-27 15:55:56 +0100

    Tree sprite order fix hack

---
+++ Assets/OrderInLayerHack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderInLayerHack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var s = GetComponent<SpriteRenderer>();
+
+        s.sortingOrder = -(int)transform.position.z;
+    }
+}
